bc1f2ee8aa8e4519b89386429f46fc25ef2e3261
Jonas Schiefner <jonas.schiefner@zweitag.de>
2021-05-18 23:18:22 +0200

    use NavigationView and alter list style

--- shopping-list-ios/ShoppingList.swift
+++ shopping-list-ios/ShoppingList.swift
@@ -11,14 +11,19 @@ struct ShoppingList: View {
     @ObservedObject var viewModel = CategoryViewModel()
     
     var body: some View {
-        List {
-            ForEach(viewModel.categories, id: \.id) { category in
-                Section(header: Text(category.name)) {
-                    ItemsView(categoryId: category.id!)
+        NavigationView {
+            List {
+                ForEach(viewModel.categories, id: \.id) { category in
+                    Section(header: Text(category.name)) {
+                        ItemsView(categoryId: category.id!)
+                    }
                 }
+            }.onAppear() {
+                viewModel.connectData()
             }
-        }.onAppear() {
-            viewModel.connectData()
+            .navigationTitle("Shopping List")
+            .navigationBarItems(leading: EditButton())
+            .listStyle(PlainListStyle())
         }
     }
 }
