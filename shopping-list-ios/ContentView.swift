23e061b14a689e5ba164583cdd7a77aea43c38e2
Jonas Schiefner <jonas.schiefner@zweitag.de>
2021-05-18 21:01:05 +0200

    Initial Commit

---
+++ shopping-list-ios/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  shopping-list-ios
+//
+//  Created by Jonas Schiefner on 18.05.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ shopping-list-ios/shopping_list_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  shopping_list_iosApp.swift
+//  shopping-list-ios
+//
+//  Created by Jonas Schiefner on 18.05.21.
+//
+
+import SwiftUI
+
+@main
+struct shopping_list_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
