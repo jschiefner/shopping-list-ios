f4df4499bfad6d6785dfae7645eca0df9eb191b2
Jonas Schiefner <jonas.schiefner@zweitag.de>
2021-05-29 23:10:45 +0200

    add rules to category

--- shopping-list-ios/Models/Category.swift
+++ shopping-list-ios/Models/Category.swift
@@ -12,6 +12,19 @@ struct Category: Identifiable, Codable, Hashable {
     @DocumentID var id: String? = UUID().uuidString
     var name: String
     var position: Float
+    var rules = [String]()
     
     static let defaultCategory = Category(id: "BqvRVayFXsRfNVbnQc2C", name: "Default", position: 10000300)
+    
+    mutating func addRule(_ rule: String) {
+        print("adding rule \(rule.lowercased()) to \(name)")
+        rules.append(rule.lowercased())
+    }
+    
+    mutating func deleteRule(_ rule: String) {
+        print("removing rule \(rule.lowercased()) from \(name)")
+        rules.removeAll { value in
+            value == rule.lowercased()
+        }
+    }
 }
